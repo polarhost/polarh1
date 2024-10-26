3ccf0bc6171aac499aa97c3ac4aac82f34a09339
polarhost <sedmanstar@gmail.com>
2024-10-27 07:16:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import random
 import asyncio
 from discord.ext import commands
 import os
-from keep_alive import keep_alive()
+from keep_alive import keep_alive
+
+keep_alive()
 
 intents = discord.Intents.default()
 intents.message_content = True
