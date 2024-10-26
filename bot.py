b1d5fd8111d2c346d5218eaf13e61b583bebf5ef
polarhost <sedmanstar@gmail.com>
2024-10-26 16:17:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 import asyncio
 from discord.ext import commands
 import os
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
