fa5d08ddf268c1705d33a966a02d78972137d493
polarhost <sedmanstar@gmail.com>
2024-10-26 19:14:14 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "Discord Bot 101 LOL!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=10000)
 
 def keep_alive():  
     t = Thread(target=run)
