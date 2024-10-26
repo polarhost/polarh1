db242cf629a7b88b036cf449fda1f64f0a4235ff
polarhost <sedmanstar@gmail.com>
2024-10-27 07:11:41 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Alive"
+    return "I'm Alive"
 
 def run():
-  app.run(host='0.0.0.0',port=10000)
+  app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():  
     t = Thread(target=run)
